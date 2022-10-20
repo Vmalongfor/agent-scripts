fd09bc76e71c2ac630632b24b9a5463ec005c786
Veroline Malongfor <vmalongfor@gmail.com>
2022-10-20 15:53:17 -0400

    package for alpine

---
+++ packages-alpine.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## author: Veroline
+#### Date: 10/19/2022
+#### This is a script to install packages on alpine
+
+
+sudo apk add wget 
+sudo apk add net-utils 
+sudo apk add sysstat 
+sudo apk add finger 
+sudo apk add gcc 
+sudo apk add make 
+sudo apk add python3 
+sudo apk add epel-release 
+sudo apk add git
+sleep 10
+echo "Installation of packages on alpine is completed"
