136d9392fc53e81c1b97fd4339d736a9fe13b98c
Veroline Malongfor <vmalongfor@gmail.com>
2022-10-20 16:25:06 -0400

    ubuntu packages for installation

---
+++ package-ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## author: Veroline
+#### Date: 10/19/2022
+#### This is a script to install packages on ubuntu.
+
+
+sudo pt install wget -y
+sudo apt install net-utils -y
+sudo apt install sysstat -y
+sudo apt install finger -y
+sudo apt install gcc -y
+sudo apt install make -y
+sudo apt install python3 -y
+sudo apt install epel-release -y
+sudo apt install git -y
+sleep 10
+echo "Installation of packages on ubuntu is completed"
