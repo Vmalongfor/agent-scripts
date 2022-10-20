9285c10f87e13b60d174cfab96f24d4a437cbea8
Veroline Malongfor <vmalongfor@gmail.com>
2022-10-20 17:06:22 -0400

    script to install packages on centos7

--- packages-centos7.sh
+++ packages-centos7.sh
@@ -4,14 +4,14 @@
 #### This is a script to install packages on centos7.
 
 
-yum install wget -y
-yum install net-utils -y
-yum install sysstat -y
-yum install finger -y
-yum install gcc -y
-yum install make -y
-yum install python3 -y
-yum install epel-release -y
-yum install git -y
-sleep 10
+sudo yum install wget -y
+sudo yum install net-utils -y
+sudo yum install sysstat -y
+sudo yum install finger -y
+sudo yum install gcc -y
+sudo yum install make -y
+sudo yum install python3 -y
+sudo yum install epel-release -y
+sudo yum install git -y
+sleep 5
 echo "Installation of packages on centos7 is completed"
